cb816e33f3692c86bed5ab62aa9e50661c41e51a
VovaKyrychuk <kyrychuk81@gmail.com>
2024-06-05 01:07:00 +0300

    some change

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { contactsRouter } from './routers/contacts.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -20,13 +20,28 @@ export const setupServer = () => {
     }),
   );
 
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    res.status(200).json({
+      data: contact,
+    });
+  });
+
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello world!',
     });
   });
-
-  app.use(contactsRouter);
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
