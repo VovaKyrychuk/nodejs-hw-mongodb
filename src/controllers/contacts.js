6c57af578a20fdf5d0fa3b8ea1e12c13e74bd349
VovaKyrychuk <kyrychuk81@gmail.com>
2024-06-15 23:57:23 +0300

    step 1 complete

---
+++ src/controllers/contacts.js
@@ -0,0 +1,30 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getContactsController = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
+
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  if (!contact) {
+    next(new Error('Contact not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
--- src/server.js
+++ src/server.js
@@ -20,49 +20,51 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
+  // app.get('/contacts', async (req, res) => {
+  //   try {
+  //     const contacts = await getAllContacts();
 
-      res.status(200).json({
-        status: '200',
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: '500',
-        message: 'Failed to fetch contacts',
-        error: error.message,
-      });
-    }
-  });
+  //     res.status(200).json({
+  //       status: '200',
+  //       message: 'Successfully found contacts!',
+  //       data: contacts,
+  //     });
+  //   } catch (error) {
+  //     res.status(500).json({
+  //       status: '500',
+  //       message: 'Failed to fetch contacts',
+  //       error: error.message,
+  //     });
+  //   }
+  // });
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-      const { contactId } = req.params;
-      const contact = await getContactById(contactId);
+  // app.get('/contacts/:contactId', async (req, res) => {
+  //   try {
+  //     const { contactId } = req.params;
+  //     const contact = await getContactById(contactId);
 
-      if (contact) {
-        res.status(200).json({
-          status: '200',
-          message: `Successfully found contact with id ${contactId}!`,
-          data: contact,
-        });
-      } else {
-        res.status(404).json({
-          status: '404',
-          message: `Contact with id ${contactId} not found`,
-        });
-      }
-    } catch (error) {
-      res.status(500).json({
-        status: '500',
-        message: 'Failed to fetch contact',
-        error: error.message,
-      });
-    }
-  });
+  //     if (contact) {
+  //       res.status(200).json({
+  //         status: '200',
+  //         message: `Successfully found contact with id ${contactId}!`,
+  //         data: contact,
+  //       });
+  //     } else {
+  //       res.status(404).json({
+  //         status: '404',
+  //         message: `Contact with id ${contactId} not found`,
+  //       });
+  //     }
+  //   } catch (error) {
+  //     res.status(500).json({
+  //       status: '500',
+  //       message: 'Failed to fetch contact',
+  //       error: error.message,
+  //     });
+  //   }
+  // });
+
+  app.use(contactsRouter);
 
   app.get('/', (req, res) => {
     res.json({
