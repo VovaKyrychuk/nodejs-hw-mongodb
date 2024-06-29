1a471cbafd75b37c43e183041d72a21cb4218b91
VovaKyrychuk <kyrychuk81@gmail.com>
2024-06-30 01:17:54 +0300

    validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,6 +7,11 @@ import {
   deleteContactController,
   updateContactController,
 } from '../controllers/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  contactCreateSchema,
+  contactUpdateSchema,
+} from '../validation/contacts.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -14,7 +19,16 @@ const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post(
+  '/contacts',
+  validateBody(contactCreateSchema),
+  ctrlWrapper(createContactController),
+);
+router.patch(
+  '/contacts/:contactId',
+  validateBody(contactUpdateSchema),
+  ctrlWrapper(updateContactController),
+);
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactController));
+
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,52 @@
+import Joi from 'joi';
+
+export const contactCreateSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Name should be a string',
+    'string.min': 'Name should have at least {#limit} characters',
+    'string.max': 'Name should have at most {#limit} characters',
+    'any.required': 'Name is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Phone number should be a string',
+    'string.min': 'Phone number should have at least {#limit} characters',
+    'string.max': 'Phone number should have at most {#limit} characters',
+    'any.required': 'Phone number is required',
+  }),
+  email: Joi.string().email().min(3).max(20).messages({
+    'string.email': 'Email must be a valid email address',
+    'string.min': 'Email should have at least {#limit} characters',
+    'string.max': 'Email should have at most {#limit} characters',
+  }),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal')
+    .messages({
+      'string.base': 'Contact type should be a string',
+      'any.only': 'Contact type must be one of [work, home, personal]',
+    }),
+});
+
+export const contactUpdateSchema = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Name should be a string',
+    'string.min': 'Name should have at least {#limit} characters',
+    'string.max': 'Name should have at most {#limit} characters',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).messages({
+    'string.base': 'Phone number should be a string',
+    'string.min': 'Phone number should have at least {#limit} characters',
+    'string.max': 'Phone number should have at most {#limit} characters',
+  }),
+  email: Joi.string().email().min(3).max(20).messages({
+    'string.email': 'Email must be a valid email address',
+    'string.min': 'Email should have at least {#limit} characters',
+    'string.max': 'Email should have at most {#limit} characters',
+  }),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal').messages({
+    'string.base': 'Contact type should be a string',
+    'any.only': 'Contact type must be one of [work, home, personal]',
+  }),
+}).or('name', 'phoneNumber', 'email', 'isFavourite', 'contactType');
