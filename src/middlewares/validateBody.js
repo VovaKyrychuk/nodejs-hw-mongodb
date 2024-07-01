f3009bb19d276d816c1dea596844bcf3dfca074a
VovaKyrychuk <kyrychuk81@gmail.com>
2024-07-01 22:26:53 +0300

    Update ValidateBody

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,15 +1,29 @@
+// import createHttpError from 'http-errors';
+
+// export const validateBody = (schema) => async (req, res, next) => {
+//   try {
+//     await schema.validateAsync(req.body, {
+//       abortEarly: false,
+//     });
+//     next();
+//   } catch (err) {
+//     const error = createHttpError(400, error.details[0].message, {
+//       errors: err.details,
+//     });
+//     next(error);
+//   }
+// };
 import createHttpError from 'http-errors';
 
-export const validateBody = (schema) => async (req, res, next) => {
-  try {
-    await schema.validateAsync(req.body, {
-      abortEarly: false,
-    });
+export const validateBody = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      const httpError = createHttpError(400, error.details[0].message, {
+        expose: true,
+      });
+      return next(httpError);
+    }
     next();
-  } catch (err) {
-    const error = createHttpError(400, error.details[0].message, {
-      errors: err.details,
-    });
-    next(error);
-  }
+  };
 };
