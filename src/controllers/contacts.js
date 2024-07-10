1f82c607ff43453b751ef4fff99f3ccb45cfcb43
VovaKyrychuk <kyrychuk81@gmail.com>
2024-07-10 20:01:41 +0300

    add log

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -12,6 +12,7 @@ import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getContactsController = async (req, res, next) => {
   try {
+    console.log('Request Query Params:', req.query);
     const { page, perPage } = parsePaginationParams(req.query);
     const { sortBy, sortOrder } = parseSortParams(req.query);
     const filter = parseFilterParams(req.query);
