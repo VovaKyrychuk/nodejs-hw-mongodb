66d43a5474e7d69321226595ac36d014e39fb6d8
VovaKyrychuk <kyrychuk81@gmail.com>
2024-07-13 20:39:55 +0300

    Update validated.js

--- src/middlewares/validateId.js
+++ src/middlewares/validateId.js
@@ -1,10 +1,34 @@
+// import { isValidObjectId } from 'mongoose';
+// import createHttpError from 'http-errors';
+
+// export const validateId = (req, res, next) => {
+//   const { contactId } = req.params;
+//   if (!isValidObjectId(contactId)) {
+//     return next(HttpError(404, 'Not found'));
+//   }
+//   next();
+// };
+
 import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
-export const validateId = (req, res, next) => {
-  const { contactId } = req.params;
-  if (!isValidObjectId(contactId)) {
-    return next(HttpError(404, 'Not found'));
-  }
-  next();
-};
+const validateId =
+  (idName = 'id') =>
+  (req, res, next) => {
+    try {
+      const id = req.params[idName];
+
+      if (!id) {
+        throw new Error('ID in validateId is not provided');
+      }
+
+      if (!isValidObjectId(id)) {
+        return next(createHttpError(400, 'Invalid ID'));
+      }
+      return next();
+    } catch (error) {
+      return next(createHttpError(500, error.message));
+    }
+  };
+
+export default validateId;
