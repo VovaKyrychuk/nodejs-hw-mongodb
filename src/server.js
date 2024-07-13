05b9c209b6172c959dd2d5303a72c36a9e83d4d2
VovaKyrychuk <kyrychuk81@gmail.com>
2024-07-13 21:36:36 +0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -30,6 +30,13 @@ export const setupServer = () => {
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
